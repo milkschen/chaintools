965826cf8668c5c65216234bd7a5b87fce5f9e32
Nae-Chyun Chen <s93rm6@gmail.com>
2025-06-21 17:48:18 -0700

    Deprecate python 3.7 in the next release (#24)
    Shows a warning if using python <3.8

---
+++ chaintools/__init__.py
@@ -0,0 +1,13 @@
+import sys
+import logging
+
+if sys.version_info < (3, 8):
+    logging.warning(
+        "chaintools will require Python 3.8 or higher in the next release. "
+        "Current version: %s",
+        sys.version,
+    )
+    # raise RuntimeError(
+    #     "chaintools requires Python 3.8 or higher. "
+    #     f"Current version: {sys.version}"
+    # )
